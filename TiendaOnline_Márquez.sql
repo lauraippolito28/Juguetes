fcca7f9342be61efa47189d5e8fd6516fdebd169
lunaamars <148171358+lunaamars@users.noreply.github.com>
2024-10-02 10:34:18 -0500

    Update TiendaOnline_Márquez.sql

--- TiendaOnline_Márquez.sql
+++ TiendaOnline_Márquez.sql
@@ -54,55 +54,53 @@ references Usuario(idUsuario);
 
 alter table Producto_Venta
 add constraint FKProducto
-foreign key (idPoductoFK)
+foreign key (idProducto)
 references Producto(idProducto);
 
 alter table Producto_Venta
-add constraint FKOrden
-foreign key (idOrdenFK)
-references Orden(idOrden);
+add constraint FKVenta
+foreign key (idVentaFK)
+references Venta(idVenta);
 
 /*Punto 5*/
 describe Venta;
 describe Cliente_To;
+describe Producto;
 select * from Usuario;
 select * from Producto;
 select * from Venta;
 select * from Cliente_TO;
 
 insert into Cliente_TO values('',1111112,'Luna',31056894,'Marzo'),(1,1111113,'Sofía',31056857,'Abril'),(2,11111879,'Diego',31057684,'Junio'),(3,1111114,'Camilo',31083684,'Julio');
-insert into Producto_O values('',1569,'Chaqueta azul',65.000),(11,6895,'Silla reclinable',186.000),(22,6384,'Libro de colorear',16.000),(33,0935,'Caja Chicles',3.200);
+insert into Producto values('',1569,'Chaqueta azul',6,65.000),(11,6895,'Silla reclinable',25,186.000),(22,6384,'Libro de colorear',21,16.000),(33,0935,'Caja Chicles',110,3.200);
 insert into Usuario values('',659843,'Cajero',4,1),(111,364859,'Personal Bodega',2,6),(112,659843,'Cajero',6,3),(113,959334,'Administrador',1,9);
-insert into Venta values(1,111,1,11,65.000,'Octubre'),(2,113,4,33,186.000,'Febrero');
+insert into Venta values('',100,111,1,6384,65.000,'3-octubre'),(56,100,111,1,6384,65.000,'3-octubre');
 
 /*Punto 6*/
-
-select * from Producto_O;
+select * from Producto;
 
 /*25/8*/
-
 select sum(totalVenta) from Venta;
-select avg(precio) from Producto;
-
-select * from Venta inner join Cliente on Venta.idCliente_TO=Cliente.idCliente_TO;
-
-select p.*,c.nombreCliente, v.max(precio)
-from Precio p
-inner join cliente c on p,idPrecio = c. idPrecioFK;
+select avg(precioporProducto) from Producto;
+select * from Venta inner join Cliente_TO on Venta.idCliente_TOFK=Cliente_TO.idCliente_TO;
+DESCRIBE cLIENTE_TO;
+select p.*,c.nombreCliente_TO, v.max(precioporProducto)
+from Precio.p
+inner join cliente c on p.idPrecio=c.idPrecioFK;
 
 /*Consultar el cliente de la máxima venta hecha*/
 select c.*, nombreCliente_TO, Venta.idCliente_TOFK, Producto_Venta.totalVentaFK
 from orden
 inner join Cliente_TO on Cliente_TO.idCliente_TO = venta.Cliente_TOFK
 inner join Producto_Venta on venta.idVenta = Producto_Venta.idVentaFK
-where total = max(total)
+where total = max(total);
 
 /*Consultar el usuario y cliente de una venta específica*/
 select Cliente_TO.nombreCliente_TO, Venta.idCliente_TOFK, Usuario.nombreUsuario
-fom Venta
+fRom Venta
 join Cliente_TO on Cliente_TO.idCliente_TO = Venta.idCliente_TOfk
 join Usuario on Usuario.idUsuario = Venta.idUsuariofk
-whwere Venta.idVenta = 111;
+where Venta.idVenta = 111;
 
 /*Consultar los productos que compró un cliente específico*/
 select Producto.nombreProducto, Producto_Venta.cantidad, Producto_Venta.totalVenta
@@ -114,4 +112,23 @@ where Venta.idCliente = 111;
 /*Consultar todos los clientes que han hecho compras*/
 select Cliente_TO.nombreCliente
 from Cliente_TO
-join Venta on Cliente_TO.idCliente_TO = Venta.idCliente_TO
+join Venta on Cliente_TO.idCliente_TO = Venta.idCliente_TO;
+
+/*27-9*/
+insert into Cliente_TO values(4,1111812,'Tatiana',310846894,'Agosto');
+insert into Producto values(44,8669,'Coche grande',8,230.000);
+insert into Usuario values(114,959334,'Luna Administradora',1,1);
+insert into Venta values(49,161,114,4,8669,230.000,'25-septiembre');
+
+describe Venta;
+update Cliente_TO set nombreCliente_TO='Tatiana Cabrera Vargas' where idCliente_TO=4;
+update Cliente_TO set fechaNacimiento='24/11/1985' where idCliente_TO=4;
+update Cliente_TO set telefonoCliente_TO=3144606978 where idCliente_TO=4;
+update Cliente_TO set telefonoCliente_TO=3144606978 where idCliente_TO=4;
+insert into Cliente_TO values(55163118,1111812,'Tatiana Cabrera Vargas',3144606978,24/11/24);
+
+select * from Cliente_TO;
+select * from venta;
+
+update Venta set idCliente_TOFK=55163118 where idVenta=49;
+delete from Cliente_TO where idCliente_TO=4;
